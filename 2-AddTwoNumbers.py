67ee07ea57219796b07f5400a51ab7bd1e88222c
alanchrissantony <alanchrissantony@gmail.com>
2024-06-25 23:57:26 +0530

    2

---
+++ 2-AddTwoNumbers.py
@@ -0,0 +1,26 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        l1_sum = ""
+        l2_sum = ""
+        current = l1
+        while current or l2:
+            if current:
+                l1_sum+=str(current.val)
+                current = current.next
+            if l2:
+                l2_sum+=str(l2.val)
+                l2 = l2.next
+        s=(str(int(l1_sum[::-1]) + int(l2_sum[::-1]))[::-1])
+        print(s)
+        current = l1
+
+        result=ListNode()
+        i=0
+        while i<len(s):
+            current=result
+            
+            while current.next:
+                current = current.next
+            current.next = ListNode(s[i])
+            i+=1
+        return result.next
