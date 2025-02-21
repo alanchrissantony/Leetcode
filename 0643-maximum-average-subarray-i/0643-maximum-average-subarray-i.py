751c8a93e62a318f267068dffd686d889b56abdf
Alan Chris Antony <alanchrissantony@gmail.com>
2025-02-21 18:36:58 +0530

    Time: 84 ms (58.27%), Space: 27.4 MB (78.51%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,7 +3,6 @@ class Solution:
         sm=sum(nums[:k])
         mx=sm
         for i in range(k, len(nums)):
-            print(sm)
             sm+=nums[i]
             sm-=nums[i-k]
             mx=max(mx, sm) 
