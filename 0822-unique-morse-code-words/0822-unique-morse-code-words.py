efeb7d81cde3841d3155e3b54d469f816675028c
Alan Chris Antony <alanchrissantony@gmail.com>
2025-02-27 13:05:02 +0530

    Time: 0 ms (100%), Space: 17.8 MB (37.92%) - LeetHub

--- 0822-unique-morse-code-words/0822-unique-morse-code-words.py
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.py
@@ -1,11 +1,9 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
         dx=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        dy={}
+        dy=dict(zip(string.ascii_lowercase, dx))
         sy={}
     
-        for i, el in enumerate(string.ascii_lowercase):
-            dy[el]=dx[i]
         for word in words:
             rst=""
             for el in word:
