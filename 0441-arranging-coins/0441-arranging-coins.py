9f398ab5b18877750b6dc538d95a6edd5e6c7368
Alan Chris Antony <alanchrissantony@gmail.com>
2025-03-06 15:34:32 +0530

    Time: 587 ms (13.49%), Space: 17.9 MB (11.98%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        cnt=0
+        for i in range(1, n+1):
+            n-=i
+            if n>=0:
+                cnt+=1
+            else:
+                break
+        return cnt
