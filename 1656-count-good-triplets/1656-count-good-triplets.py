e92d227f291e14f31f48116e96d71c0000c0544d
Alan Chris Antony <alanchrissantony@gmail.com>
2025-03-20 12:02:05 +0530

    Time: 181 ms (87.42%), Space: 17.6 MB (97.7%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.py
+++ 1656-count-good-triplets/1656-count-good-triplets.py
@@ -4,9 +4,10 @@ class Solution:
         dx=0
         for i in range(n-2):
             for j in range(i+1,n-1):
-                for k in range(j+1, n):
-                    if abs(arr[i] - arr[j])<=a and abs(arr[j] - arr[k])<=b and abs(arr[i] - arr[k])<=c:
-                        dx+=1
+                if abs(arr[i] - arr[j])<=a:
+                    for k in range(j+1, n):
+                        if abs(arr[j] - arr[k])<=b and abs(arr[i] - arr[k])<=c:
+                            dx+=1
         return dx
 
 
