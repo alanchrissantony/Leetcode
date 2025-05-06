555ca83ccc6aefa05955c0a3e6a5c4d4253c32a8
Alan Chris Antony <alanchrissantony@gmail.com>
2025-05-06 15:44:16 +0530

    Time: 3 ms (22.65%), Space: 17.7 MB (65.85%) - LeetHub

---
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/1698-replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def modifyString(self, s: str) -> str:
+        char="abcdefghijklmnopqrstuvwxyz"
+        for i, el in enumerate(s):
+            if el=='?':
+                for j, alp in enumerate(char):
+                    if i>0 and alp in s[i-1] or i<len(s)-1 and alp in s[i+1]:
+                        continue
+                    s=s[:i]+alp+s[i+1:]
+        return s
