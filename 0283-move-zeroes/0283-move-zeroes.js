4b64638df25501f928f46a520b9a42de8ccc6196
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-05 03:57:21 +0530

    Time: 12 ms (11.95%), Space: 60.7 MB (30.98%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let n=0
+    for(let i=0; i<nums.length-n; i++){
+        if(nums[i]==0){
+            nums.splice(i,1)
+            nums.push(0)
+            i--
+            n++
+        }
+    }
+    return nums
+};
