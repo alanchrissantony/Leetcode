d344c7535e80e09f4711703e0abf4f13de287ff5
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-05 03:05:39 +0530

    Time: 0 ms (100%), Space: 54.8 MB (19.35%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    n=needle.length
+    let dx=haystack.slice(0, n)
+    for(let i=0; i<(haystack.length)-(n)+1; i++){
+        if(dx==needle){
+            return i
+        }
+        dx=dx.slice(1) + haystack[n+i]
+    }
+    return -1
+};
