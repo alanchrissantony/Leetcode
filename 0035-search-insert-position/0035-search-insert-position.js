ea803f4cfb28949b434bdbb65d79fc590ae69ff1
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-05 09:00:54 +0530

    Time: 0 ms (100%), Space: 53.7 MB (63.58%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let i;
+    for(i=0; i<nums.length; i++){
+        if(target<=nums[i]){
+            return i
+        }
+    }
+    return i
+};
