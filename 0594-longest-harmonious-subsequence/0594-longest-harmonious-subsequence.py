7f17c7e3712244ffc9dbd963b85fb8ec5c9b3498
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-07 11:01:39 +0530

    Time: 16 ms (93.81%), Space: 19.4 MB (12.06%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -3,12 +3,8 @@ class Solution:
         dt=Counter(nums)
         mx=0
         for el in dt:
-            if el+1 in dt and el-1 in dt:
-                mx=max(mx, dt[el]+max(dt[el+1], dt[el-1]))
-            elif el+1 in dt:
+            if el+1 in dt:
                 mx=max(mx, dt[el]+dt[el+1])
-            elif el-1 in dt:
-                mx=max(mx, dt[el]+dt[el-1])
         return mx
 
         
