cd57755effca2fed92ebe5860ed9dbb96b250584
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-16 09:45:55 +0530

    Time: 11 ms (94.44%), Space: 17.9 MB (56.27%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
@@ -1,18 +1,13 @@
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
-        n=len(fruits)
-        f_idx=b_idx=k=0
-        while f_idx<n-k:
-            b_idx=0
-            while b_idx<n-k:
-                if baskets[b_idx]>=fruits[f_idx]:
-                    del fruits[f_idx]
-                    del baskets[b_idx]
-                    k+=1
-                    f_idx-=1
-                    break
-                b_idx+=1
-            f_idx+=1
+        unp=len(baskets)
 
-        return len(baskets)
+        for fruit in fruits:
+            for basket in baskets:
+                if basket>=fruit:
+                  baskets.remove(basket)
+                  unp-=1
+                  break
+        return unp
+
 
