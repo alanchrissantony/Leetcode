7dc08d23f5ff10097794df616e5032475c474b5a
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-23 15:20:39 +0530

    Time: 3 ms (62.2%), Space: 17.7 MB (59.31%) - LeetHub

---
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        n=len(nums)
+        cnt=0
+        for ldx in reversed(range(n)):
+            if nums[ldx]-diff in nums and nums[ldx]-diff*2 in nums:
+                cnt+=1
+        return cnt
