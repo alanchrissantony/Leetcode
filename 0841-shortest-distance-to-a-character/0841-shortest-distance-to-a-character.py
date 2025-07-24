569f30f6c2f7ffc2fc58b47de1aaa556fe6ee24f
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-24 10:11:16 +0530

    Time: 2 ms (71.41%), Space: 17.8 MB (44.27%) - LeetHub

--- 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
@@ -1,14 +1,32 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        sp=set()
-        for idx, el in enumerate(s):
-            if el == c:
-                sp.add(idx+1)
-        dp=[0]*len(s)
-        for inx, el in enumerate(s):
-            mn=float('inf')
-            for it in sp:
-                mn=min(mn, abs(it-(inx+1)))
-            dp[inx]=mn
+        n=len(s)
+        lft=[float("inf")]*n
+        rgt=[float("inf")]*n
+        dp=[0]*n
+
+        dx=float("inf")
+        for idx in range(n):
+            if s[idx] == c:
+                dx=0
+                lft[idx]=dx
+            else:
+                if dx != float("inf"):
+                    dx+=1
+                    lft[idx]=dx
+        
+        dx=float("inf")
+        for idx in range(n-1, -1, -1):
+            if s[idx] == c:
+                dx=0
+                rgt[idx]=dx
+            else:
+                if dx != float("inf"):
+                    dx+=1
+                    rgt[idx]=dx
+        for idx in range(n):
+            dp[idx]=min(lft[idx], rgt[idx])
         return dp
+
+
         
