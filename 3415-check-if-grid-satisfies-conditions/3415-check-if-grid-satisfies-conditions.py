3463b84584c0a869b1857b5842ac39f0372f8d8c
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-24 09:29:05 +0530

    Time: 0 ms (100%), Space: 17.8 MB (36.77%) - LeetHub

--- 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.py
+++ 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.py
@@ -1,17 +1,10 @@
 class Solution:
     def satisfiesConditions(self, grid: List[List[int]]) -> bool:
-        flag=True
-        m=len(grid)
+        m,n=len(grid),len(grid[0])
         for i in range(m):
-            flag=True
-            n=len(grid[i])
             for j in range(n):
                 if i<m-1 and grid[i][j] != grid[i + 1][j]:
-                    flag=False
-                    break
+                    return False
                 if j<n-1 and grid[i][j] == grid[i][j + 1]:
-                    flag=False
-                    break
-            if not flag:
-                break
-        return flag
+                    return False
+        return True
