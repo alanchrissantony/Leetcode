7be1a75407668426ef0a1632d527f655da2c4520
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-25 16:16:26 +0530

    Time: 139 ms (93.71%), Space: 18 MB (82.57%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
@@ -1,18 +1,23 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
         n=len(s)
-        dt={'a':0, 'b':0, 'c':0}
-        for el in s:
-            dt[el]+=1
-        if min(dt.values())<k:
-            return -1
-        dx=float("inf")
-        ldx=0
-        for rdx in range(n):
-            dt[s[rdx]]-=1
-            while min(dt.values())<k:
-                dt[s[ldx]]+=1
-                ldx+=1
-            dx=min(dx, n-(rdx-ldx+1))
-        return dx
+
+        if n < 3*k: return -1
+        dp=[0]*3
+
+        for char in s:
+            dp[ord(char)-ord('a')]+=1
+
+        if min(dp)<k: return -1
+
+        lft=0
+        for rgt, char in enumerate(s):
+            dp[ord(char)-ord('a')]-=1
+            if min(dp)<k:
+                dp[ord(s[lft])-ord('a')]+=1
+                lft+=1
+        return n-(rgt-lft+1)
+            
+
+
 
