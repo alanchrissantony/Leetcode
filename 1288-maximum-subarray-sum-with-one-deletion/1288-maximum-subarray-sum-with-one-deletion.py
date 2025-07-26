4c339b1a2a2947a431b60ff246a8c21be7c34c7d
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-27 00:43:43 +0530

    Time: 48 ms (38.79%), Space: 25.9 MB (94.59%) - LeetHub

--- 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.py
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.py
@@ -4,7 +4,7 @@ class Solution:
         dy=mx=arr[0]
         for idx in range(1,len(arr)):
             dx=max(dy, dx+arr[idx])
-            dy=max(arr[idx]+dy, arr[idx])
-            curr=max(dx, dy)
-            mx=max(mx, curr)
+            dy=max(dy+arr[idx], arr[idx])
+            curr=max(dx,dy)
+            mx=max(mx,curr)
         return mx
