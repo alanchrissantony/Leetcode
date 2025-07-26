460ccaf16bad02b926195767cd038c2d88a856c2
Alan Chris Antony <alanchrissantony@gmail.com>
2025-07-27 00:41:02 +0530

    Time: 47 ms (42.97%), Space: 26.1 MB (78.83%) - LeetHub

---
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumSum(self, arr: List[int]) -> int:
+        dx=0
+        dy=mx=arr[0]
+        for idx in range(1,len(arr)):
+            dx=max(dy, dx+arr[idx])
+            dy=max(arr[idx]+dy, arr[idx])
+            curr=max(dx, dy)
+            mx=max(mx, curr)
+        return mx
